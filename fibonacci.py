06e06556d70bf9202f3740ea01c5ee480ec50739
princzhemanth <82358599+princzhemanth@users.noreply.github.com>
2021-04-30 14:49:10 +0530

    Update fibonacci.py
    hemapriyatharsan

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,16 @@
-num=int(input("ENTER THE VALUES HERE :"))
-F1,F2=0,1
-F3=F1+F2
-print("THE VALUE OF FIRST",num, "is");
-print(F1)
-print(F2)
-for i in range (4):
-    print(F3)
-    F1=F2
-    F2=F3
-    F3=F1+F2
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
